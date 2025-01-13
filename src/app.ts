5c272ef149c99c443d3f620984f3f9a352169b49
Sebastian Solano <sebaxsolanom@gmail.com>
2025-01-13 13:52:12 -0500

    added: model for the database

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.config.ts";
 
 dotenv.config();
 
 const app = express();
 
-// Middleware
-app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.json({ limit: "15mb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
